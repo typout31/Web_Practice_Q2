8d633524555847357249546d6c0e8503e810b07a
Nick <nicholas.chu98@gmail.com>
2021-11-24 11:17:12 -0800

    starter files

---
+++ main.js
@@ -0,0 +1,14 @@
+/*
+ * Project:
+ * File Name: main.js
+ * Description:
+ *
+ * Created Date:
+ * Author:
+ *
+ */
+
+const IOhandler = require("./IOhandler"),
+  zipFilePath = `${__dirname}/myfile.zip`,
+  pathUnzipped = `${__dirname}/unzipped`,
+  pathProcessed = `${__dirname}/grayscaled`;
