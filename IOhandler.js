a80afcddcfb4789019691d46074c685b3cccc628
Nick <nicholas.chu98@gmail.com>
2021-12-01 01:12:46 -0800

    Main has issues but functions work

--- IOhandler.js
+++ IOhandler.js
@@ -8,6 +8,8 @@
  *
  */
 
+const { dir } = require("console");
+const { resolve } = require("path");
 const unzipper = require("unzipper"),
   fs = require("fs"),
   PNG = require("pngjs").PNG,
@@ -20,7 +22,23 @@ const unzipper = require("unzipper"),
  * @param {string} pathOut
  * @return {promise}
  */
-const unzip = (pathIn, pathOut) => {};
+const unzip = (pathIn, pathOut) => {
+  if (fs.existsSync(pathOut)) {
+    console.log("File Path Exists");
+  } else {
+    console.log("create")
+    return new Promise((resolve, reject) => {
+      fs.createReadStream(`${pathIn}`)
+      .pipe(unzipper.Extract({ path: `${pathOut}` }))
+      .on("finish", function () {
+        resolve("Extraction operation complete")})
+      .on("error", function() {
+        resolve("Extraction operation complete")
+      })
+    })
+}};
+
+// unzip("myfile.zip", "unzipped");
 
 /**
  * Description: read all the png files from given directory and return Promise containing array of each png file path
@@ -28,7 +46,26 @@ const unzip = (pathIn, pathOut) => {};
  * @param {string} path
  * @return {promise}
  */
-const readDir = (dir) => {};
+const readDir = (dir) => {
+  return new Promise((resolve, reject) => {
+    const png = []
+    fs.readdir(dir, function(err, images) {
+      if (err) {
+        reject(err);
+      } else {
+        for (let image of images) {
+          if (path.extname(image) === ".png") {
+            png.push(`${image}`)
+          }
+        }
+        console.log(png)
+        resolve(png)
+      }
+    })
+  });
+};
+
+readDir("./unzipped");
 
 /**
  * Description: Read in png file by given pathIn,
@@ -38,7 +75,31 @@ const readDir = (dir) => {};
  * @param {string} pathProcessed
  * @return {promise}
  */
-const grayScale = (pathIn, pathOut) => {};
+
+const grayScale = (image, pathOut) => {
+  var fs = require("fs"),
+    PNG = require("pngjs").PNG;
+  
+  fs.createReadStream(`${image}`)
+    .pipe(
+    new PNG()
+    )
+    .on("parsed", function () {
+      for (var y = 0; y < this.height; y++) {
+        for (var x = 0; x < this.width; x++) {  
+          var idx = (this.width * y + x) << 2;        
+          var gray = (this.data[idx] + this.data[idx + 1] + this.data[idx + 2]) / 3;
+          // grayscale
+          this.data[idx] = gray;
+          this.data[idx + 1] = gray;
+          this.data[idx + 2] = gray;
+        }
+      }
+      this.pack().pipe(fs.createWriteStream(`./${pathOut}/grayscaled${x}.png`))
+    })
+};
+
+grayScale("./unzipped/in.png", "./grayscaled");
 
 module.exports = {
   unzip,
--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 /*
- * Project:
+ * Project: Practice Final Question 2
  * File Name: main.js
- * Description:
+ * Description: main file that runs iohandler
  *
- * Created Date:
- * Author:
+ * Created Date: Nov 29
+ * Author: Nicholas Chu
+ * 
  *
  */
 
+const { unzip } = require("unzipper");
+
 const IOhandler = require("./IOhandler"),
   zipFilePath = `${__dirname}/myfile.zip`,
-  pathUnzipped = `${__dirname}/unzipped`,
+  pathUnzipped = `unzipped`,
   pathProcessed = `${__dirname}/grayscaled`;
+
+IOhandler.unzip(zipFilePath, pathUnzipped)
+  .then((msg) => console.log(msg))
+  .then((data) => IOhandler.readDir(pathUnzipped))
+  .then((data) => data.forEach(image => {
+    IOhandler.grayScale(`${pathUnzipped}/${image}`, pathProcessed)
+  .catch((err) => console.log(err))
+}))
+
